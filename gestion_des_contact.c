45fddd5beb0ce66cec88b9480b71047f3bb19be1
youco . <lionme55i8bo@gmail.com>
2025-09-08 12:26:33 +0100

    testing projet

--- gestion_des_contact.c
+++ gestion_des_contact.c
@@ -1,21 +1,139 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 struct contact
 {
     char nom[50];
     char email[50];
-    int telephone[20];
+    char telephone[20];
 };
 
+int n = 0;
+int contact_a_ajouter;
+
+void ajouter( struct contact con[])
+{
+    printf("entrez le nombre des contact a ajouter: ");
+    scanf("%d", &contact_a_ajouter);
+    getchar();
+
+    for (int i = 0; i < contact_a_ajouter + n; i++)
+    {
+        printf("\nentrez le nom de %d contact: ", i + 1);
+        fgets(con[i].nom, 50, stdin);
+        con[i].nom[strcspn(con[i].nom, "\n")] = 0;
+
+        printf("\nentrez l'email de %d contact: ", i + 1);
+        fgets(con[i].email, 50, stdin);
+        con[i].email[strcspn(con[i].email, "\n")] = 0;
+
+        printf("\nentrez le numero de %d contact: ", i + 1);
+        scanf("%s", con[i].telephone);
+
+        getchar();
+    }
+    n += contact_a_ajouter;
+}
+void suprimer(struct contact con[])
+{
+    
+    int found = 0;
+    char contact_a_suprimer[50];
+    printf("entrez le nom du contact a suprimer");
+    fgets(contact_a_suprimer, 50, stdin);
+    contact_a_suprimer[strcspn(contact_a_suprimer, "\n")] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(con[i].nom, contact_a_suprimer) == 0)
+        {
+            for (int j = i; i < n - 1; j++)
+            {
+                con[j] = con[j + 1];
+            }
+            n--;
+            found = 1;
+            printf("\nle contact est suprimmer");
+        }
+    }
+    if (!found)
+    {
+        printf("\ncontact non trouvé");
+    }
+}
+void modifier(struct contact con[50])
+{
+    char contact_a_chercher[50];
+    int found = 0;
+    printf("entrez le nom du contact a modifier: ");
+    fgets(contact_a_chercher, 50, stdin);
+    contact_a_chercher[strcspn(contact_a_chercher, "\n")] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(con[i].nom, contact_a_chercher) == 0)
+        {
+            printf("\nl'email actuel: %s", con[i].email);
+            printf("\nle nouveau email: ");
+            fgets(con[i].email, 50, stdin);
+            con[i].email[strcspn(con[i].email, "\n")] = 0;
+
+            printf("\nle numero actuel: %s", con[i].telephone);
+            printf("\nle nouveau numero: ");
+            scanf("%d", &con[i].telephone);
+            getchar();
+            found = 1;
+            printf("\nle contact mise à jour");
+            break;
+        }
+    }
+    if (!found)
+    {
+        printf("\ncontact non trouvé");
+    }
+}
+void afficher(struct contact con[])
+{
+    printf("\n*** Liste des contacts ***\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("le contact %d:\n", i + 1);
+        printf("  nom: %s\n", con[i].nom);
+        printf("  email: %s\n", con[i].email);
+        printf("  telephone: %s\n", con[i].telephone);
+    }
+    if (n == 0)
+    {
+        printf("votre liste est vide");
+    }
+}
+void chercher(struct contact con[])
+{
+    char contact_a_chercher[50];
+    // struct contact con[50];
+    int found = 0;
+    printf("Entrez le nom du contact a chercher: ");
+    fgets(contact_a_chercher, 50, stdin);
+    contact_a_chercher[strcspn(contact_a_chercher, "\n")] = 0; // remove newline
+
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(con[i].nom, contact_a_chercher) == 0)
+        {
+            printf("telephone et l'email de ce contact sont: \n");
+            printf("%s\n", con[i].email);
+            printf("%s\n", con[i].telephone);
+            found = 1;
+        }
+    }
+    if (!found)
+    {
+        printf("contact introuvable");
+    }
+}
 int main()
 {
     struct contact con[50];
     int choix;
-    int n = 0;
-    int contact_a_ajouter;
-    char contact_a_chercher[50];
-    int found = 0;
     do
     {
         int found = 0;
@@ -33,115 +151,23 @@ int main()
         switch (choix)
         {
         case 1:
-            printf("entrez le nombre des contact a ajouter: ");
-            scanf("%d", &contact_a_ajouter);
-            getchar();
-
-            for (int i = 0; i < contact_a_ajouter + n ; i++)
-            {
-                printf("\nentrez le nom de %d contact: ", i +1);
-                fgets(con[i].nom, 50, stdin);
-                con[i].nom[strcspn(con[i].nom,"\n")] = 0;
-
-                printf("\nentrez l'email de %d contact: ", i +1);
-                fgets(con[i].email, 50, stdin);
-                con[i].email[strcspn(con[i].email,"\n")] = 0;
-
-                printf("\nentrez le numero de %d contact: ", i +1);
-                scanf("%d", con[i].telephone);
-                getchar();
-            }
-            n += contact_a_ajouter;
+            ajouter(con);
             break;
-        
         case 2:
-            
-            printf("entrez le nom du contact a modifier: ");
-            fgets(contact_a_chercher, 50, stdin);
-            contact_a_chercher[strcspn(contact_a_chercher, "\n")] = 0;
-            
-            for (int i = 0; i < n; i++)
-            {
-                if (strcmp(con[i].nom, contact_a_chercher) == 0)
-                {
-                    printf("\nl'email actuel: %s",con[i].email);
-                    printf("\nle nouveau email: ");
-                    fgets(con[i].email, 50, stdin);
-                    con[i].email[strcspn(con[i].email, "\n")] = 0;
-
-                    printf("\nle numero actuel: %s",con[i].telephone);
-                    printf("\nle nouveau numero: ");
-                    scanf("%d", con[i].telephone);
-                    getchar();
-                    found = 1;
-                    printf("\nle contact mise à jour");
-                    break;
-                }
-                
-            }
-            if (!found)
-                {
-                    printf("\ncontact non trouvé");
-                }
+            modifier(con);
             break;
         case 3:
-            char contact_a_suprimer[50];
-            printf("entrez le nom du contact a suprimer");
-            fgets(contact_a_suprimer, 50, stdin);
-            contact_a_suprimer[strcspn(contact_a_suprimer, "\n")] = 0;
-            for (int i = 0; i < n; i++)
-            {
-                if (strcmp(con[i].nom, contact_a_suprimer) == 0)
-                {
-                    for (int j = i; i < n - 1; j++)
-                    {
-                        con[j] = con[j + 1];
-                    }
-                    n--;
-                    found = 1;
-                    printf("\nle contact est suprimmer");
-                    break;
-                }
-            }
-            if (!found)
-                {
-                    printf("\ncontact non trouvé");
-                }
+            suprimer(con);
             break;
         case 4:
-            printf("\n*** Liste des contacts ***\n");
-                for (int i= 0; i < n; i++) 
-                {
-                    printf("le contact %d:\n", i + 1);
-                    printf("  nom: %s\n", con[i].nom);
-                    printf("  email: %s\n", con[i].email);
-                    printf("  telephone: %d\n", con[i].telephone);
-                }
+          afficher(con);
             break;
         case 5:
-            printf("Entrez le nom du contact a chercher: ");
-                fgets(contact_a_chercher, 50, stdin);
-                contact_a_chercher[strcspn(contact_a_chercher, "\n")] = 0;//remove newline
-                
-                for (int i = 0; i < n; i++) {
-                    if (strcmp(con[i].nom, contact_a_chercher) == 0)
-                    {
-                        printf("telephone et l'email de ce contact sont: \n");
-                        printf("%s\n", con[i].email);
-                        printf("%d\n", con[i].telephone);
-                        found = 1;
-                        
-                    }
-                }
-                    if (!found)
-                    {
-                        printf("contact introuvable");
-                    }
-                    break;
+            chercher(con);
+            break;
         case 0:
-            printf("au revoir");
-                    
+            printf("au revoir!");
+            break;
         }
     } while (choix != 0);
-    
 }
